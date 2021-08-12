4575c0681f8d8c70decf69d50d160f49d6b4f6fe
Alina Melnyk <alinkaaa345@gmail.com>
2021-08-13 01:09:52 +0300

    Изменение хедера при переходе по кнопкам

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,1 +1,13 @@
 //FT-03 По нажатию на кнопку "My Library" отрисовывается библиотека пользователя
+const btnMyLibrary = document.querySelector('.library');
+const btnHome = document.querySelector('.home');
+const header = document.querySelector('.page-header');
+
+
+btnMyLibrary.addEventListener('click', changeLibraryThemeHeader);
+
+function changeLibraryThemeHeader() {
+    header.classList.add('library-header');
+    btnMyLibrary.classList.add('current');
+    btnHome.classList.remove('current');
+}
