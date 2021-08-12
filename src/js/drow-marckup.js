cc54b8e2dad8c84a0f9419d4ef1829da220db3d2
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-08-12 14:57:08 +0300

    add new poster

--- src/js/drow-marckup.js
+++ src/js/drow-marckup.js
@@ -36,8 +36,8 @@ function filterGalleryProperty(filmResult) {
 }
 function filterForPosters(film) {
       if (film.poster_path === null) {
-           film.poster_path = `https://www.begindot.com/wp-content/uploads/2019/01/Best-Coming-Soon-HTML-Templates.jpg`;
-        //   film.poster_path = `../images/Soon.jpg`;
+        //    film.poster_path = `https://www.begindot.com/wp-content/uploads/2019/01/Best-Coming-Soon-HTML-Templates.jpg`;
+          film.poster_path = `https://i.ibb.co/5KRPDGk/viber-2021-08-12-14-52-03-837.jpg`;
             console.log(`ooops`)
        } else {
             film.poster_path = `https://image.tmdb.org/t/p/original${film.poster_path}`
